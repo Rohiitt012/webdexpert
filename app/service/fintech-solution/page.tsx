e0e3720f8c368bc207c8ad3cf724ff4e6e59967c
Rohiitt012 <rohiitt786@gmail.com>
2025-12-19 15:59:30 +0530

    Add FAQ section (Section12) above footer for fintech-solution page

--- app/service/fintech-solution/page.tsx
+++ app/service/fintech-solution/page.tsx
@@ -11,6 +11,7 @@ import Section8 from "@/components/sections/home/fintech-solution/Section8";
 import Section9 from "@/components/sections/home/fintech-solution/Section9";
 import Section10 from "@/components/sections/home/fintech-solution/Section10";
 import Section11 from "@/components/sections/home/fintech-solution/Section11";
+import Section12 from "@/components/sections/home/fintech-solution/Section12";
 export default function Home() {
     return (
         <>
@@ -27,6 +28,7 @@ export default function Home() {
                 <Section9 />
                 <Section10 />
                 <Section11 />
+                <Section12 />
             </Layout>
         </>
     );
