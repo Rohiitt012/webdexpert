b818ae7e3669a08c366c67f0fe7d7c1c3a7ae3da
Rohiitt012 <rohiitt786@gmail.com>
2025-12-19 16:22:35 +0530

    Add Section3a and Section3b to crm-development page

--- app/service/crm-development/page.tsx
+++ app/service/crm-development/page.tsx
@@ -3,6 +3,8 @@ import StatsTicker from "@/components/common/StatsTicker";
 import Section1 from "@/components/sections/home/fintech-solution/Section1";
 import Section2 from "@/components/sections/home/fintech-solution/Section2";
 import Section3 from "@/components/sections/home/fintech-solution/Section3";
+import Section3a from "@/components/sections/home/fintech-solution/Section3a";
+import Section3b from "@/components/sections/home/fintech-solution/Section3b";
 import Section5 from "@/components/sections/home/fintech-solution/Section5";
 import Section6 from "@/components/sections/home/fintech-solution/Section6";
 import Section7 from "@/components/sections/home/fintech-solution/Section7";
@@ -15,6 +17,8 @@ export default function Home() {
                 <StatsTicker />
                 <Section2 />
                 <Section3 />
+                <Section3a />
+                <Section3b />
                 <Section5 />
                 <Section6 />
                 <Section7 />
