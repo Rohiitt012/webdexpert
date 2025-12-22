975320ef259a498a4450e0fcdab3e23ade1e4271
Rohiitt012 <rohiitt786@gmail.com>
2025-12-23 02:45:35 +0530

    Fix build error: remove Section4b import from startup page

--- app/service/startup/page.tsx
+++ app/service/startup/page.tsx
@@ -4,7 +4,6 @@ import Section1 from "@/components/sections/home/startup/Section1";
 import Section2 from "@/components/sections/home/startup/Section2";
 import Section3 from "@/components/sections/home/startup/Section3";
 import Section4 from "@/components/sections/home/startup/Section4";
-import Section4b from "@/components/sections/home/startup/Section4b";
 import Section5 from "@/components/sections/home/startup/Section5";
 import Section6 from "@/components/sections/home/startup/Section6";
 import Section7 from "@/components/sections/home/startup/Section7";
@@ -18,7 +17,6 @@ export default function Home() {
                 <Section2 />
                 <Section3 />
                 <Section4 />
-                <Section4b />
                 <Section5 />
                 <Section6 />
                 <Section7 />
